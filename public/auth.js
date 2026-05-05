8ac8502c2c9621b9bbeca3711d1d94c9060ef14e
devang1374 <vagheladevang123@gmail.com>
2026-05-05 22:35:38 +0530

    added buffer for api call

--- public/auth.js
+++ public/auth.js
@@ -2,6 +2,8 @@ const BASE_URL = "https://taskapp-laravel.onrender.com/api";
 
 //login
 async function login() {
+    showLoader();
+
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
 
@@ -21,10 +23,14 @@ async function login() {
     }else{
         alert(data.message);
     }
+
+    hideLoader();
 }
 
 //register
 async function register() {
+    showLoader();
+
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
@@ -40,4 +46,14 @@ async function register() {
     let data = await res.json();
     alert(data.message);
 
+    hideLoader();
+
+}
+
+function showLoader() {
+  document.getElementById("loader").style.display = "flex";
+}
+
+function hideLoader() {
+  document.getElementById("loader").style.display = "none";
 }
